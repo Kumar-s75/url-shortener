e83dc5a2d91752778533d139b9658d9ea1e6f697
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-08-23 16:36:13 +0530

    implemented data model

---
+++ src/main/java/com/viiku/urlshortener/url/model/payload/request/UrlRequest.java
@@ -0,0 +1,18 @@
+package com.viiku.urlshortener.url.model.payload.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class UrlRequest {
+
+    @NotNull(message = "Original URL can not be blank")
+    private String originalUrl;
+
+    private String customAlias;
+    private LocalDateTime expiryDate;
+}
